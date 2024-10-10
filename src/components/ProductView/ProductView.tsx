2f8c42d056dcc214711d2fa49bf84d5da5c6d9eb
rexsekkai <rexsekkai@users.noreply.github.com>
2024-10-10 13:24:48 +0000

    deploy: 77a210149a1f772ae5fa9afd168c22848afc3e07

--- src/components/ProductView/ProductView.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-import { useLoaderData } from "react-router";
-
-export default function ProductView() {
-    const data = useLoaderData();
-    useEffect(() => console.log("data", data), [data]);
-    return <div>ProductView</div>;
-}
--- src/routes/HomeLoader.tsx
+++
@@ -1,6 +0,0 @@
-import { API_URL } from "../constants";
-
-export const getHome = async () => {
-    const data = await fetch(`${API_URL}/landing`);
-    return data;
-};
